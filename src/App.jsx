e1afe0df35e6b074ca11eb686c54612ea2486ac3
Faizal Kusmawan <faizalindrak@gmail.com>
2025-12-18 13:49:57 +0700

    feat: Initialize React app with routing for Home and BOMViewer pages and a conditional Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import BOMViewer from './pages/BOMViewer';
 
+function AppContent() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === '/viewer';
+
+  return (
+    <div className="App">
+      {!hideNavbar && <Navbar />}
+      <div className={hideNavbar ? '' : 'pt-16'}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/viewer" element={<BOMViewer />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="pt-16">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/viewer" element={<BOMViewer />} />
-          </Routes>
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 }
